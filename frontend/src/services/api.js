21d8e6e6c3fc434ef3736bef7d4facd29056102e
Laxman-Raut <rjayshila@gmail.com>
2026-06-30 17:28:15 +0530

    updated production fixes

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://crm-software-p0xs.onrender.com/api",
 });
 
 api.interceptors.request.use((config) => {
